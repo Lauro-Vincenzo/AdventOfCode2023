9ef7227f21db303fccc40673c3c542337e9504c0
Vincenzo-Lauro <laurovincenzo.097@gmail.com>
2023-12-01 17:32:54 +0100

    Reading from file OK

---
+++ december-1/first-challenge/src/main.rs
@@ -0,0 +1,13 @@
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    read_file("input.txt");
+}
+
+fn read_file(path : &str) -> String{
+    let mut file = File::open(path).expect("Unable to find file");
+    let mut file_content = String::new();
+    file.read_to_string(&mut file_content).expect("Unable to read from file");
+    return file_content;
+}
