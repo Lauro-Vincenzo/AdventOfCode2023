626559075b9960e085564a6c05730bb00b273a25
Vincenzo-Lauro <laurovincenzo.097@gmail.com>
2023-12-01 19:05:41 +0100

    First working version

--- december-1/first-challenge/src/main.rs
+++ december-1/first-challenge/src/main.rs
@@ -2,7 +2,41 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() {
-    read_file("input.txt");
+    let calibration_document = read_file("input.txt");
+
+    let mut sum : i32 = 0;
+
+    for line in calibration_document.lines(){
+        let first_char_digit = find_first_digit(&line);
+        let second_char_digit = find_second_digit(&line);
+        let final_digit = first_char_digit.to_string() + &second_char_digit.to_string();
+        let digit = final_digit.parse::<i32>().expect("Conversion Failed!");
+        sum += digit;
+    }
+
+    println!("Sum is: {sum}")
+}
+
+fn find_first_digit(string_to_parse : &str) -> char{
+    let char_digit_result = string_to_parse.chars().find(|c| c.is_digit(10));
+    
+    let char_digit = match char_digit_result {
+        Some(value) => value,
+        None => '.'
+    };
+
+    return char_digit;
+}
+
+fn find_second_digit(string_to_parse : &str) -> char{
+    let char_digit_result = string_to_parse.chars().rev().find(|c| c.is_digit(10));
+    
+    let char_digit = match char_digit_result {
+        Some(value) => value,
+        None => '.'
+    };
+
+    return char_digit;
 }
 
 fn read_file(path : &str) -> String{
