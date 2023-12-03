09b7338a4f68bcb9736556bed2adc58ed79db42c
Vincenzo Lauro <vincenzo@zuru.com>
2023-12-03 15:33:47 +0100

    Parsing of games ok

--- december-2/src/main.rs
+++ december-2/src/main.rs
@@ -18,7 +18,26 @@ struct Game {
 fn main() {
     let game_record = read_file("input.txt");
     let game_entries = game_record.lines().map(String::from).collect();
-    let _ = parse_games(&game_entries);
+    let games = parse_games(&game_entries);
+
+    print_games(games);
+}
+
+fn print_games(games: Vec<Game>) {
+    for game in games {
+        print!("Game ");
+        print!("{}:", game.game_id);
+        print!(" ");
+
+        for set in game.sets {
+            for extraction in set.extractions {
+                print!(
+                    "{} {} ",
+                    extraction.number_of_cubes, extraction.type_of_cube
+                );
+            }
+        }
+    }
 }
 
 fn parse_games(game_lines: &Vec<String>) -> Vec<Game> {
@@ -40,21 +59,6 @@ fn parse_games(game_lines: &Vec<String>) -> Vec<Game> {
     return games;
 }
 
-fn parse_cube_sets(game_entry: &String) -> Vec<CubeSet> {
-    let mut cube_sets: Vec<CubeSet> = Vec::new();
-
-    let game_init_char_index = game_entry.find(':').unwrap();
-    let (_, cube_sets_substr) = game_entry.split_at(game_init_char_index);
-    let cube_set_entries: Vec<String> = cube_sets_substr.split(';').map(String::from).collect();
-
-    for cube_set_entry in cube_set_entries {
-        let cube_set = parse_cube_set(&cube_set_entry);
-        cube_sets.push(cube_set);
-    }
-
-    return cube_sets;
-}
-
 fn parse_game_id(game_entry: &String) -> i32 {
     let game_entry_init = game_entry.split(':').next().unwrap_or("");
     let game_entry_id = game_entry_init
@@ -66,32 +70,46 @@ fn parse_game_id(game_entry: &String) -> i32 {
     return game_entry_id.parse::<i32>().expect("Conversion Failed!");
 }
 
+fn parse_cube_sets(game_entry: &String) -> Vec<CubeSet> {
+    let mut cube_sets: Vec<CubeSet> = Vec::new();
+
+    let game_init_char_index = game_entry.find(':').unwrap();
+    let (_, cube_sets_substr) = game_entry.split_at(game_init_char_index + 1);
+
+    let cube_set_entries: Vec<String> = cube_sets_substr.split(';').map(String::from).collect();
+
+    for cube_set_entry in cube_set_entries {
+        let cube_set = parse_cube_set(&cube_set_entry);
+        cube_sets.push(cube_set);
+    }
+
+    return cube_sets;
+}
+
 fn parse_cube_set(cube_set_entry: &String) -> CubeSet {
     let cube_extractions_data: Vec<String> = cube_set_entry.split(',').map(String::from).collect();
-    let mut extractions: Vec<CubeExtraction> = Vec::new();
+    let mut cubes_extractions: Vec<CubeExtraction> = Vec::new();
 
     for cube_extraction_data in cube_extractions_data {
         let whitespace_index = cube_extraction_data
+            .trim()
             .find(' ')
             .expect("No whitespace found!");
-        println!("{cube_extraction_data}");
-        //println!("{whitespace_index}");
 
-        // let (number_of_cubes, type_of_cube) = cube_extraction_data.split_at(whitespace_index);
+        let (number, color) = cube_extraction_data.trim().split_at(whitespace_index + 1);
 
-        // println!("{number_of_cubes}");
-
-        // let cube_extraction = CubeExtraction {
-        //     number_of_cubes: number_of_cubes.parse::<i32>().expect("Conversion failed!"),
-        //     type_of_cube: type_of_cube.to_string(),
-        // };
-        // extractions.push(cube_extraction);
+        let cube_extraction = CubeExtraction {
+            number_of_cubes: number.trim().parse::<i32>().expect("Conversion failed!"),
+            type_of_cube: color.to_string(),
+        };
+        cubes_extractions.push(cube_extraction);
     }
 
     return CubeSet {
-        extractions: extractions,
+        extractions: cubes_extractions,
     };
 }
+
 fn read_file(path: &str) -> String {
     let mut file = File::open(path).expect("Unable to find file");
     let mut file_content = String::new();
